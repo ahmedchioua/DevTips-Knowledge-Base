e807b71b61ff7d6eeea3aba194a71c478662f314
ahmedchioua <ahmedchioua@gmail.com>
2017-06-11 21:33:01 +0000

    adding messages and validation

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator =require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 //connect to the database 
 
@@ -35,7 +38,38 @@ app.use(bodyParser.json());
 //Set Public Folder
 app.use(express.static(path.join(__dirname,'public')));
 
+//Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+  }));
 
+//Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Express Validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+//Home route
 app.get('/', function (req,res) {
 	
 	Article.find({},function(err,articles){
@@ -54,7 +88,28 @@ app.get('/', function (req,res) {
 	
 });
 
-//add website route
+//Get Single Article Route
+app.get('/article/:id',function(req,res) {
+	Article.findById(req.params.id,function (err,article) {
+		res.render('article',{
+		article:article
+	});
+	});
+});
+
+//Edit Article Route 
+app.get('/article/edit/:id',function(req,res) {
+	Article.findById(req.params.id,function (err,article) {
+		res.render('edit_article',{
+		title:'Edit Article',
+		article:article
+		
+	});
+	});
+});
+
+
+//add article route
 app.get('/articles/add',function(req,res){
 	res.render('add_article',{
 		title:'add article to knowledge base'
@@ -77,13 +132,45 @@ app.post('/articles/add',function(req,res){
 			return;
 		}
 		else{
+			res.flash('sucess','Article Added');
 			res.redirect('/');
-		}
-		
-
+		}	
 	});
 });
 
+//Update Submit POST Route
+
+app.post('/article/edit/:id',function(req,res){
+	let article = {};
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+
+	let query = {_id:req.params.id}
+
+	Article.update(query,article,function(err){
+		if(err){
+			console.log(err);
+			return;
+		}
+		else{
+			res.redirect('/');
+		}
+		
+	});
+});
+
+//Delete Article Route
+app.delete('/article/:id',function (req,res) {
+	let query = {_id:req.params.id}
+
+	Article.remove(query,function (err) {
+		if(err){
+			console.log(err);
+		}
+		res.send('Sucess');
+	});
+});
 app.listen(3000,function(){
 	console.log('server started on port 3000 ...')
 });
